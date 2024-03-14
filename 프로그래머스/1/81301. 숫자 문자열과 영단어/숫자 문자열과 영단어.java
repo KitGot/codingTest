308724a440aa6eb52b7211ccbf0774d53912f281
ChoJW <113075719+KitGot@users.noreply.github.com>
2024-03-14 15:53:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 13.90 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,43 +1,53 @@
 class Solution {
-    public int solution(String s) {
-    String trans = "";
-    String str = "";
-        
-    char array[] = s.toCharArray();
-        
-    for(int i = 0;i<array.length;i++){
-        // 숫자일 때
-        if(array[i] <= '9'){
-        if(!trans.equals("")){
-            str += change(trans) + "";
-            trans = "";
-        }
-        str += array[i] + "";
-        }
-        // 문자일 때
-        else if(array[i] >= 'a'){
-            trans += array[i] + "";
-            if(!change(trans).equals("")){
-                str += change(trans) + "";
-                trans = "";
-            }
-        }
-    }
-        
-    return Integer.parseInt(str);
-      
-}
-    public String change(String str){
-        if(str.equals("zero")) return "0";
-        else if(str.equals("one")) return "1";
-        else if(str.equals("two")) return "2";
-        else if(str.equals("three")) return "3";
-        else if(str.equals("four")) return "4";
-        else if(str.equals("five")) return "5";
-        else if(str.equals("six")) return "6";
-        else if(str.equals("seven")) return "7";
-        else if(str.equals("eight")) return "8";
-        else if(str.equals("nine")) return "9";
-        else return "";
-    }
+public int solution(String s) {
+		int beforelen = 0;
+		String answer = "";
+		String check = "";
+		char[] arr = s.toCharArray();
+		for (char c : arr) {
+			if (c >= '0' & c <= '9') {
+				answer += c;
+			} else {
+
+				check += c;
+
+				answer += checking(check);
+				if (answer.length() > beforelen) {
+					check = "";
+				}
+			}
+			beforelen = answer.length();
+		}
+
+		return Integer.parseInt(answer);
+	}
+
+	public String checking(String str) {
+		switch (str) {
+		case "one":
+			return "1";
+		case "zero":
+			return "0";
+		case "two":
+			return "2";
+		case "three":
+			return "3";
+		case "five":
+			return "5";
+		case "four":
+			return "4";
+		case "six":
+			return "6";
+		case "seven":
+			return "7";
+		case "eight":
+			return "8";
+		case "nine":
+			return "9";
+		default:
+			return "";
+
+		}
+
+	}
 }
