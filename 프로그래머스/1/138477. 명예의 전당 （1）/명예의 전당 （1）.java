28f8d0009dfaea84517e925834b02c4627f8e101
ChoJW <113075719+KitGot@users.noreply.github.com>
2024-03-19 14:08:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.24 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,31 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-        List<Integer>  answer = new ArrayList<>(); // 매주 최하위 순위 리스트
-        List<Integer> list = new ArrayList<>();// 명예의 전당 리스트
-        int min = 0;
-        // k번만큼 score을 answer에 추가
+        int[] answer = new int[score.length];
+        int min = 0; // 명예의 전당의 최소값
+        // 명예의 전당 리스트
+        List<Integer> list = new ArrayList<>();
+        // score 배열 순회
         for(int i = 0;i<score.length;i++){
-            // 명예의 전당 리스트 사이즈가 k보다 작으면 걍 넣고
-            if(list.size() < k){
-                list.add(score[i]);
-                min = list.stream().mapToInt(t -> t).min().getAsInt();
-                answer.add(min);
-            }else{
-                // top k에 score가 들어가지 못하면
-                if(min > score[i]){
-                    answer.add(min);
-                    continue;
-                }else{
-                    // top k에 score가 들어가면 기존 꺼 삭제
-                    list.remove(list.indexOf(min));    
-                }
-                list.add(score[i]);                   
-                min = list.stream().mapToInt(t -> t).min().getAsInt();
-                answer.add(min);
-            }
+        // 리스트 사이즈가 k보다 작으면 명예의 전당에 넣기
+        if(list.size() < k){
+            list.add(score[i]);
+        // 명예의 전당의 최소값 보다 크다면 명예의 전당에 넣고 원래있던 최소값 빼기
+        }else if(min < score[i]){
+            // 최소값 빼기
+            list.remove(list.indexOf(min));
+            //  score 명예의 전당에 넣기
+            list.add(score[i]);
         }
+        // 최소값 갱신
+        min = list.stream().mapToInt(t -> t).min().getAsInt();
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        answer[i] = min;
+        }
+        return answer;
     }
 }
