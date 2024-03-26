a70ef6bdb8811e29cb9443f6673b7d2875d0ece8
ChoJW <113075719+KitGot@users.noreply.github.com>
2024-03-26 14:48:31 +0900

    [level 1] Title: 덧칠하기, Time: 1.79 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,34 +1,30 @@
 class Solution {
-    // n은 구역의 개수
-    // m은 룰러의 길이
-    // section은 칠해야하는 구역을 배열형태
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        // 칠해진 구역 배열
-        boolean[] flag = new boolean[n+1];
+        // 칠했는지 확인 배열
+        boolean check[] = new boolean[n+1];
+        // 칠하는 인덱스
         int index = 0;
-        // section이 하나거나 룰러의 길이랑 구역의 길이가 같으면 한번만에 가능
+        // 만약 n 과 m이 같거나 section이 하나면 1리턴
         if(n == m | section.length == 1) return 1;
         
         // section 배열 순회
-        for(int sections : section){
-            index = sections;
-            if(flag[sections]){
+        for(int now : section){
+            index = now;
+            // 칠해져있으면 칠하지 않고 넘기기
+            if(check[index]){
                 continue;
             }else{
-                
-            if(sections + m < flag.length){
-                while(index < sections + m){
-                    flag[index++] = true;
+                // 칠하는 인덱스가 sectino 길이보다 크면 그냥 다칠해버리기
+                if(index + m > n){
+                    for(int i = index;i<check.length;i++) check[i] = true;
+                }else{
+            // 칠해져 있지 않다면 현재 인덱스 부터 m 만큼 칠하고 칠한 횟수 하나 추가
+                for(int i = index;i < index + m;i++) check[i] = true;
                 }
-            }else{
-                while(index < flag.length) flag[index++] = true;
-            }
-                    
                 answer++;
             }
         }
-        
         return answer;
     }
 }
