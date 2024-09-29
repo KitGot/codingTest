316e7d3901113807e42b23b3e46ad2d443893464
ChoJW <113075719+KitGot@users.noreply.github.com>
2024-09-29 15:49:06 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String []args)throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(stk.nextToken()) + Integer.parseInt(stk.nextToken()));
+    }
+}
