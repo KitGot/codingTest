3c56dfbf168a12f707366b03ab0d45109020a2c3
ChoJW <113075719+KitGot@users.noreply.github.com>
2024-11-06 21:11:40 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,21 +1,19 @@
 class Solution {
     public int solution(int n) {
-        if(n == 1) return 1;
         int answer = 0;
-        int second = n;
-        int first = 1;
         
-        while(first < second){
-            if(n % first == 0){
-                second = n / first;
-                if(second == first){ // 나눴을때 first == target이면 종료
-                    answer += second;
-                    return answer;
-                }
-                answer += second + first;
+        int sqrt = (int)Math.sqrt(n);
+        
+        
+        for(int i = 1;i<=sqrt;i++){
+            if(n % i == 0){
+                answer += i; 
+                if(n / i != i) answer += (n / i);
+        
             }
-                first++;            
-        }
+            }
+        
+        
         return answer;
- }
+    }
 }
